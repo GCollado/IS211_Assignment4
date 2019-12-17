3dfcdb6568ceade2b62a36d3f6f569357eb50a61
GCollado <gabriel.collado@spsmail.cuny.edu>
2019-12-16 23:43:18 -0500

    Update search_compare.py

--- search_compare.py
+++ search_compare.py
@@ -1,101 +1,176 @@
 #! usr/bin/env python
 #! -*- coding: utf-8 -*-
+"""
+====================================
+Part 1 - Search Algorithm Comparison
+====================================
+1. Create a 'search_compare.py' file.
+2. Create 4 functions using the codes from the 'Sequential and Binary Search'
+ section:
+    a. sequential_search
+    b. ordered_sequential_search
+    c. binary_search_iterative
+    d. binary_search_recursive
+3. Modify each code to calculate the time required to return the results
+4. The program's main function is to return how long the function takes to
+ return the results on average. Generate 100 random positive integer input lists
+ of size 500, 1000, and 10000. Find the average run time of the 100 lists.
+    a. Generate 100 lists each list set containing 500, 100, and 10000.
+    b. Using worst-case scenario, search for an element we know that will not be
+     in the list. Use -1 to search.
+    c. The results should display: 'Sequential Search Took % 10.7f seconds to run
+     on average.'
+    d. The binary and ordered sequential search functions the lists should be
+    sorted before searching.
+"""
 
-"""
-1. Create search_compare.py
-2. Create four functions: sequential_search, ordered_sequential_search, binary_search_initiative,
-and binary_search_recursive
-3. Using Search and Binary search codes, modify each function to calculate how long it takes and
-the results of the search function
-4. Main function should print the average of each function;
-    generate 100 random input lists containing positive numbers of size 500, 1000, and 1,000;
-    worst-case scenario should contain do a search for a number not on the list
-5.
-"""
-import timeit
-from timeit import Timer
 import random
+import timeit
 
-#500, 1000, 10000
 
-#should have a 100 list containing, 500, 1000, 1000
-def sequential_search(number_list, number):
+def sequential_search(search_number, size):
+    lists_of_numbers = [[random.randint(1,100) for number in range(100)] for number_list in range(size)]
     position = 0
     found = False
+    for number_list in lists_of_numbers:
+        while search_number < len(number_list) and not found:
+            if number_list[position] == search_number:
+                found = True
+            else:
+                position += 1
+        return found
 
-    while position < len(number_list) and not found:
-        if number_list[position] == number:
-            found = True
-        else:
-            position = position + 1
+
+def ordered_sequential_search(search_number, size):
+    lists_of_numbers = [[random.randint(1, 100) for number in range(100)] for number_list in range(size)]
+    position = 0
+    found = False
+    for number_list in lists_of_numbers:
+        number_list = sorted(number_list)
+        while position < len(number_list) and not found:
+            if number_list[position] == search_number:
+                found = True
+            else:
+                position += 1
     return found
 
 
-def ordered_sequential_search(number_list, number):
+def binary_search_iterative(search_number, size):
+    lists_of_numbers = [[random.randint(1, 100) for number in range(100)] for number_list in range(size)]
+    found = False
+    for number_list in lists_of_numbers:
+        first_number, last_number = 0, len(number_list) - 1
+        number_list = sorted(number_list)
+        while first_number <= last_number and not found:
+            middle = (first_number + last_number)//2
+            if number_list[middle] == search_number:
+                found = True
+            elif search_number < number_list[middle]:
+                last_number = middle - 1
+            else:
+                first_number = middle + 1
+        return found
+
+
+def binary_search_recursive(search_number, size):
+    lists_of_numbers = [[random.randint(1, 100) for number in range(100)] for number_list in range(size)]
+    for number_list in lists_of_numbers:
+        number_list = sorted(number_list)
+        if len(number_list) == 0:
+            return False
+        else:
+            middle = len(number_lists)//2
+        if number_list[middle] == search_number:
+            return True
+        elif search_number < number_list[middle]:
+            return binary_search_recursive(search_number, number_list[:middle])
+        else:
+            return binary_search_recursive(search_number, number_list[middle + 1])
+
+
+setup = """
+import random
+"""
+ss = """
+def sequential_search(search_number, size):
+    lists_of_numbers = [[random.randint(1,100) for number in range(100)] for number_list in range(size)]
     position = 0
     found = False
-    stop = False
+    for number_list in lists_of_numbers:
+        while search_number < len(number_list) and not found:
+            if number_list[position] == search_number:
+                found = True
+            else:
+                position += 1
+        return found
+"""
 
-    while position < len(number_list) and not found and not stop:
-        if number_list[position] == number:
-            found = True
-        elif number_list[position] > number:
-            stop = True
+oss = """
+def ordered_sequential_search(search_number, size):
+    lists_of_numbers = [[random.randint(1,100) for number in range(100)] for number_list in range(size)]
+    position = 0
+    found = False
+    for number_list in lists_of_numbers:
+        number_list = sorted(number_list)
+        while position < len(number_list) and not found:
+            if number_list[position] == search_number:
+                found = True
+            else:
+                position += 1
+        return found
+"""
+
+bsi = """
+number_lists = [[random.randint(1,100) for number in range(100)] for number_list in range(100)]
+
+def binary_search_iterative(search_number, lists_of_numbers=number_lists):
+    found = False
+    for number_list in lists_of_numbers:
+        first_number, last_number = 0, len(number_list) - 1
+        number_list = sorted(number_list)
+        while first_number <= last_number and not found:
+            middle = (first_number + last_number)//2
+            if number_list[middle] == search_number:
+                found = True
+            elif search_number < number_list[middle]:
+                last_number = middle - 1
+            else:
+                first_number = middle + 1
+        return found
+"""
+
+bsr = """
+def binary_search_recursive(search_number, size):
+    lists_of_numbers = [[random.randint(1,100) for number in range(100)] for number_list in range(size)]
+    for number_list in lists_of_numbers:
+        number_list = sorted(number_list)
+        if len(number_list) == 0:
+            return False
         else:
-            position = position + 1
-    return found
+            middle = len(number_lists)//2
+        if number_list[middle] == search_number:
+            return True
+        elif search_number < number_list[middle]:
+            return binary_search_recursive(search_number, number_list[:middle])
+        else:
+            return binary_search_recursive(search_number, number_list[middle + 1])
+"""
 
 
-def binary_search_iterative(number_list, number):
-     first = 0
-     last = len(number_list) -1
-     found = False
-     while first <= last and not found:
-         midpoint = (first + last) //2
-         if number_list[midpoint] == number:
-             found = True
-         elif number < number_list[midpoint]:
-             last = midpoint -1
-         else:
-             first = midpoint + 1
-     return found
+def main():
+    rt_avg_message = "{} Search took {} seconds to run, on average."
+    searches = [
+        ('Sequential', ss),
+        ('Ordered Sequential', oss),
+        ('Iterative Binary', bsi),
+        ('Recursive', bsr)
+        ]
+
+    for search in searches:
+        print(rt_avg_message.format(search[0], timeit.timeit(setup=setup, stmt=search[1], number=500)))
+        print(rt_avg_message.format(search[0], timeit.timeit(setup=setup, stmt=search[1], number=10000)))
+        print(rt_avg_message.format(search[0], timeit.timeit(setup=setup, stmt=search[1], number=10000)))
 
 
-def binary_search_recursive(number_list, number):
-    if len(number_list) == 0:
-        return False
-    else:
-        midpoint = len(number_list) //2
-    if number_list[midpoint] == number:
-        return True
-    elif number < number_list[midpoint]:
-        return binary_search_recursive(number_list[:midpoint], number)
-    else:
-        return binary_search_recursive(number_list[midpoint + 1:], number)
-
-list_comp = [[x for x in range(1, 100)] for x in range(100)]
-
-def  main():
-    pass
-
-ss = Timer("sequential_search(list_comp, 50)", "from __main__ import sequential_search")
-oss = Timer("ordered_sequential_search(list_comp, 50)", "from __main__ import ordered_sequential_search")
-bsi = Timer("binary_search_iterative(list_comp, 50)", "from __main__ import binary_search_iterative")
-bsr = Timer("binary_search_recursive(list_comp, 50)", "from __main__ import binary_search_recursive")
-
-#print('ss: ', ss.timeit(number=1000))
-#print('oss: ', oss.timeit(number=1000))
-#print('bsi: ', bsi.timeit(number=1000))
-#print('bsr: ', bsr.timeit(number=1000))
-
-#if __name__ == '__main__':
-#    print(sequential_search(listcomp, 101))
-
-i = 0
-for listitem in list_comp:
-    for item in listitem:
-            if item  == 50:
-                print('Number: 50', "list comp #:", list)
-
-
-#print(sequential_search(list_comp, 50))
+if __name__ == "__main__":
+    main()
